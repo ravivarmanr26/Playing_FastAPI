265f385595b633586b6671934a96cc75ed2f2c39
ravivarmanr-quantrail <ravivarman@quantrail-data.com>
2026-01-21 12:25:52 +0530

    Update exception handling in FastAPI app

--- exceptions/main.py
+++ exceptions/main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.requests import Request
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class RegisterUserRequest(BaseModel):
+    name : str
+    id : int
+
+class RegisterUserRespose(BaseModel):
+    success : bool
+    message : str
+
+users = {}
+
+@app.post("/register-user",response_model=RegisterUserRespose)
+def register_user(request : RegisterUserRequest):
+    if request.id in users:
+        raise HTTPException(status_code=422, detail= "User already exists")
+    
+    users[request.id] = request.name
+
+    return RegisterUserRespose(
+        success = True,
+        message = "The user have beed added"
+    )
+    
 
